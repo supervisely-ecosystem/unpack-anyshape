25c723bf42d3faed35127996069481ba03e2850f
Maxim Kolomeychenko <maxim.kolomeychenko@gmail.com>
2020-09-15 12:15:11 +0300

    simple test - works

--- src/unpack_anyshape.py
+++ src/unpack_anyshape.py
@@ -1,17 +1,12 @@
 import os
 import supervisely_lib as sly
 
-import numpy as np
-
 my_app = sly.AppService()
 
 task_id = os.environ["TASK_ID"]
-TEAM_ID = int(os.environ['context.teamId'])
 WORKSPACE_ID = int(os.environ['context.workspaceId'])
 PROJECT_ID = int(os.environ['modal.state.slyProjectId'])
 REMOVE_ORIGINAL_CLASS = bool(os.environ['modal.state.remove'])
-
-
 
 
 @my_app.callback("do")
@@ -29,6 +24,20 @@ def do(**kwargs):
     src_project_meta_json = api.project.get_meta(src_project.id)
     src_project_meta = sly.ProjectMeta.from_json(src_project_meta_json)
 
+    # check that project has anyshape classes
+    find_anyshape = False
+    new_classes_lst = []
+    for cls in src_project_meta.obj_classes:
+        cls: sly.ObjClass
+        if cls.geometry_type == sly.AnyGeometry:
+            find_anyshape = True
+            if REMOVE_ORIGINAL_CLASS is True:
+                continue
+        new_classes_lst.append(cls.clone())
+    dst_classes = sly.ObjClassCollection(new_classes_lst)
+    if find_anyshape is False:
+        raise RuntimeError("Project doesn't have classes with shape Any")
+
     # create destination project
     DST_PROJECT_NAME = "{} (without AnyShape)".format(src_project.name)
 
@@ -37,46 +46,31 @@ def do(**kwargs):
     sly.logger.info('Destination project is created.',
                     extra={'project_id': dst_project.id, 'project_name': dst_project.name})
 
-    dst_project_meta = sly.ProjectMeta()
+    dst_project_meta = src_project_meta.clone(obj_classes=dst_classes)
+    api.project.update_meta(dst_project.id, dst_project_meta.to_json())
 
-    def convert_annotation(src_ann):
+    def convert_annotation(src_ann, dst_project_meta):
         new_labels = []
         for idx, lbl in enumerate(src_ann.labels):
             lbl: sly.Label
-
             if lbl.obj_class.geometry_type == sly.AnyGeometry:
-                figure_geometry = type(lbl.geometry)
+                actual_geometry = type(lbl.geometry)
 
-                new_class_name = "{}_{}".format(lbl.obj_class.name, figure_geometry.geometry_name())
+                new_class_name = "{}_{}".format(lbl.obj_class.name, actual_geometry.geometry_name())
                 new_class = dst_project_meta.get_obj_class(new_class_name)
                 if new_class is None:
-                    new_class = sly.ObjClass(name=new_class_name, geometry_type=figure_geometry, color=sly.color.generate_rgb())
-                    pass
+                    new_class = sly.ObjClass(name=new_class_name,
+                                             geometry_type=actual_geometry,
+                                             color=sly.color.random_rgb())
+                    dst_project_meta = dst_project_meta.add_obj_class(new_class)
+                    api.project.update_meta(dst_project.id, dst_project_meta.to_json())
+
+                if REMOVE_ORIGINAL_CLASS is False:
+                    new_labels.append(lbl)
+                new_labels.append(lbl.clone(obj_class=new_class))
             else:
-                pass
-
-
-
-            cur_obj_class = lbl.obj_class
-            new_cls = dst_project_meta.obj_classes.get(lbl.obj_class.name)
-            if not need_convert(lbl.obj_class.geometry_type):
-                new_lbl = lbl.clone(obj_class=new_cls)
-                new_labels.append(new_lbl)
-            else:
-                if allow_render_non_spatial_for_any_shape(lbl) == False:
-                    continue
-                mask = common_img == idx
-                if np.any(mask):  # figure may be entirely covered by others
-                    g = lbl.geometry
-                    new_bmp = sly.Bitmap(data=mask,
-                                         labeler_login=g.labeler_login,
-                                         updated_at=g.updated_at,
-                                         created_at=g.created_at)
-                    new_lbl = lbl.clone(geometry=new_bmp, obj_class=new_cls)
-                    new_labels.append(new_lbl)
-
+                new_labels.append(lbl)
         return src_ann.clone(labels=new_labels)
-        pass
 
     for ds_info in api.dataset.get_list(src_project.id):
         ds_progress = sly.Progress('Processing dataset: {!r}/{!r}'.format(src_project.name, ds_info.name),
@@ -90,7 +84,7 @@ def do(**kwargs):
             ann_infos = api.annotation.download_batch(ds_info.id, img_ids)
             anns = [sly.Annotation.from_json(x.annotation, src_project_meta) for x in ann_infos]
 
-            new_anns = [convert_annotation(ann) for ann in anns]
+            new_anns = [convert_annotation(ann, dst_project_meta) for ann in anns]
 
             new_img_infos = api.image.upload_ids(dst_dataset.id, img_names, img_ids, metas=img_metas)
             new_img_ids = [x.id for x in new_img_infos]
@@ -102,8 +96,7 @@ def do(**kwargs):
 
 
 def main():
-    my_app = sly.AppService()
-    my_app.run(initial_events=[{"command": "do"}])
+    my_app.run(initial_events=[{"command": "do"}, {"command": "stop"}])
     my_app.wait_all()
 
 
